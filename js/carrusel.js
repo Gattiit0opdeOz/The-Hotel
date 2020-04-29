bd44f716db7d0a64434d825e1aa062bddd020d2a
X3R02 <x3r02mail@gmail.com>
2020-04-29 11:41:21 -0500

    trabajando en los carruseles de habitaciones

---
+++ js/carrusel.js
@@ -0,0 +1,6 @@
+//Con el event listener DOMContentLoaded ejecutamos una función
+//una vez el DOM sea cargado
+document.addEventListener('DOMContentLoaded', ()=>{
+   const carruselSlide = document.getElementById('carrusel-slide');
+   const carruselImages = document.getElementById();
+});
