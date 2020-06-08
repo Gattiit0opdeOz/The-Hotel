242bff4a3e7c9d4e1ee156ac2b0992ff9e8bad81
X3R02 <x3r02mail@gmail.com>
2020-06-08 09:45:17 -0500

    Weas corregidas xD

--- paginas/Registro/registro.php
+++ paginas/Registro/registro.php
@@ -1,10 +1,10 @@
 <?php
     $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
+    $correo = $_POST['correo'];
     $fecha = $_POST['fecha'];
     $fecha_salida = $_POST['fecha-salida'];
     $sexo = $_POST['sexo'];
 
-    echo "Tu nombre es: $nombre, email: $email, fecha de entrada: $fecha, fecha de salida $fecha_salida";
+    echo "Tu nombre es: $nombre, email: $correo, fecha de entrada: $fecha, fecha de salida $fecha_salida";
     echo "Sexo: $sexo";
 ?>
