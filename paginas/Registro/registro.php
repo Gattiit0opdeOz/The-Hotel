1d2f2f3d92b52a2ae18d5f0e060198c58446e279
X3R02 <x3r02mail@gmail.com>
2020-06-08 10:06:23 -0500

    HAciendo pagina de agradecimiento por la elección

--- paginas/Registro/registro.php
+++ paginas/Registro/registro.php
@@ -1,10 +1,82 @@
 <?php
-    $nombre = $_POST['nombre'];
-    $correo = $_POST['correo'];
-    $fecha = $_POST['fecha'];
-    $fecha_salida = $_POST['fecha-salida'];
-    $sexo = $_POST['sexo'];
+    try {
+        $nombre = $_POST['nombre'];
+        $correo = $_POST['correo'];
+        $fecha = $_POST['fecha'];
+        $fecha_salida = $_POST['fecha-salida'];
+        $sexo = $_POST['sexo'];
 
-    echo "Tu nombre es: $nombre, email: $correo, fecha de entrada: $fecha, fecha de salida $fecha_salida";
-    echo "Sexo: $sexo";
-?>
+    } catch (Throwable $th) {
+        echo $th;
+    }
+    ?>
+
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reserva.css">
+	<link rel="stylesheet" href="../../css/fonts.css">
+	<link rel="stylesheet" href="../../css/header_footer.css">
+    <title>Reserva</title>
+</head>
+<body>
+    <header class="header">
+		<div class="container logo-nav-container">
+			<a class="logo" href="../../index.html">The Hotel</a>
+			
+			<nav class="navegacion">
+				<ul>
+					<li class="icon-nav"><a href="../../index.html"><span class="icono"><i class="icon icon-home"></i> Habitaciones</a></li>
+
+					<li class="icon-nav"><a href="../../index.html#restaurantes"><span class="icono"><i class="icon icon-spoon-knife"></i> Restaurantes</a></li>
+
+					<li class="icon-nav"><a href="../../index.html#actividades_main"><span class="icono"><i class="icon icon-binoculars"></i> Actividades</a></li>
+
+					<li class="icon-nav"><a href="../../index.html#ubicacion"><span class="icono"><i class="icon icon-location"></i> Ubicación</a></li>
+
+					<li class="icon-nav"><a href="../../index.html#casino"><span class="icono"><i class="icon icon-spades"></i> Casino</a></li>
+
+				</ul>
+			</nav>
+		</div> 
+    </header>
+    <main>
+
+        <?php 
+            echo $nombre;
+        ?>
+
+    </main>
+    <footer id="footer">
+		<div id="columnas-footer">
+			<ul class="columna-footer">
+				<li class="item-footer"><a href="#">Habitaciones</a></li>
+				<li class="item-footer"><a href="#">Penthouse</a></li>
+				<li class="item-footer"><a href="#">De lujo</a></li>
+				<li class="item-footer"><a href="#">Doble</a></li>
+				<li class="item-footer"><a href="#">Sencilla</a></li>
+			</ul>
+			<ul class="columna-footer">
+				<li class="item-footer"><a href="#">Restaurantes</a></li>
+				<li class="item-footer"><a href="#">Méxicano</a></li>
+				<li class="item-footer"><a href="#">Italiano</a></li>
+				<li class="item-footer"><a href="#">Chino</a></li>
+			</ul>
+			<ul class="columna-footer">
+				<li class="item-footer"><a href="#">Actividades</a></li>
+			</ul>
+			<ul class="columna-footer">
+				<li class="item-footer"><a href="#">Casino</a></li>
+			</ul>
+		</div>
+		<hr>
+		<div class="contenedor-derechos">
+			<p>
+				&copy; The Hotel SA
+			</p>
+		</div>
+	</footer>
+</body>
+</html>
